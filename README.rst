1827ffbdf04bc4d3a281c46a1843e19b4ea4494e
wojtekolejarz <wojciech.olejarz@salesmanago.pl>
2018-07-18 21:26:30 +0200

    readme update

---
+++ README.rst
@@ -0,0 +1,208 @@
+=================
+pytest-testconfig
+=================
+
+.. image:: https://img.shields.io/pypi/v/pytest-testconfig.svg
+    :target: https://pypi.org/project/pytest-testconfig
+    :alt: PyPI version
+
+.. image:: https://img.shields.io/pypi/pyversions/pytest-testconfig.svg
+    :target: https://pypi.org/project/pytest-testconfig
+    :alt: Python versions
+
+.. image:: https://travis-ci.org/wojole/pytest-testconfig.svg?branch=master
+    :target: https://travis-ci.org/wojole/pytest-testconfig
+    :alt: See Build Status on Travis CI
+
+.. image:: https://ci.appveyor.com/api/projects/status/github/wojole/pytest-testconfig?branch=master
+    :target: https://ci.appveyor.com/project/wojole/pytest-testconfig/branch/master
+    :alt: See Build Status on AppVeyor
+
+Test configuration plugin for pytest.
+
+Based on nose-testconfig by Jesse Noller. Rewritten for pytest by Wojciech Olejarz and Bartłomiej Skrobek.
+
+----
+
+This `pytest`_ plugin was generated with `Cookiecutter`_ along with `@hackebrot`_'s `cookiecutter-pytest-plugin`_ template.
+
+
+Features
+--------
+pytest-testconfig is a plugin to the pytest test framework used for passing test-specific (or test-run specific) configuration data
+to the tests being executed.
+
+Currently configuration files in the following formats should be supported:
+
+- YAML (via `PyYAML <http://pypi.python.org/pypi/PyYAML/>`_)
+- INI (via `ConfigParser <http://docs.python.org/lib/module-ConfigParser.html>`_)
+- Pure Python (via Exec)
+- JSON (via `JSON <http://docs.python.org/library/json.html>`_)
+
+The plugin is ``meant`` to be flexible, ergo the support of exec'ing arbitrary
+python files as configuration files with no checks. The default format is
+assumed to be ConfigParser ini-style format.
+
+If multiple files are provided, the objects are merged. Later settings will
+override earlier ones.
+
+The plugin provides a method of overriding certain parameters from the command
+line (assuming that the main "config" object is a dict) and can easily have
+additional parsers added to it.
+
+A configuration file may not be provided. In this case, the config object is an
+emtpy dict. Any command line "overriding" paramters will be added to the dict.
+
+
+Requirements
+------------
+
+requires pytest>=3.5.0
+
+
+Installation
+------------
+
+You can install "pytest-testconfig" via `pip`_ from `PyPI`_::
+
+    $ python3 -m pip install --index-url https://test.pypi.org/simple/ pytest-testconfig
+
+
+Usage
+-----
+
+Tests can import the "config" singleton from testconfig::
+
+    from testconfig import config
+
+By default, YAML files parse into a nested dictionary, and ConfigParser ini
+files are also collapsed into a nested dictionary for foo[bar][baz] style
+access. Tests can obviously access configuration data by referencing the
+relevant dictionary keys::
+
+    from testconfig import config
+    def test_foo():
+        target_server_ip = config['servers']['webapp_ip']
+
+``Warning``: Given this is just a dictionary singleton, tests can easily write
+into the configuration. This means that your tests can write into the config
+space and possibly alter it. This also means that threaded access into the
+configuration can be interesting.
+
+When using pure python configuration - obviously the "sky is the the limit" -
+given that the configuration is loaded via an exec, you could potentially
+modify pytest, the plugin, etc. However, if you do not export a config{} dict
+as part of your python code, you obviously won't be able to import the
+config object from testconfig.
+
+When using YAML-style configuration, you get a lot of the power of pure python
+without the danger of unprotected exec() - you can obviously use the pyaml
+python-specific objects and all of the other YAML creamy goodness.
+
+Defining a configuration file
+-----------------------------
+
+Simple ConfigParser style::
+
+    [myapp_servers]
+    main_server = 10.1.1.1
+    secondary_server = 10.1.1.2
+
+So your tests access the config options like this::
+
+    from pytest_testconfig import config
+    def test_foo():
+        main_server = config['myapp_servers']['main_server']
+
+YAML style configuration::
+    myapp:
+        servers:
+            main_server: 10.1.1.1
+            secondary_server: 10.1.1.2
+
+And your tests can access it thus::
+
+    from pytest_testconfig import config
+    def test_foo():
+        main_server = config['myapp']['servers']['main_server']
+
+Python configuration file::
+
+    import socket
+
+    global config
+    config = {}
+    possible_main_servers = ['10.1.1.1', '10.1.1.2']
+
+    for srv in possible_main_servers:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((srv, 80))
+        except:
+            continue
+        s.close()
+        config['main_server'] = srv
+        break
+
+And lo, the config is thus::
+
+    from pytest_testconfig import config
+    def test_foo():
+        main_server = config['main_server']
+
+If you need to put python code into your configuration, you either need to use
+the python-config file faculties, or you need to use the !!python tags within
+PyYAML/YAML - raw ini files no longer have any sort of eval magic.
+
+Command line options
+--------------------
+
+After it is installed, the plugin adds the following command line flags to
+pytest::
+
+    --tc-file=TESTCONFIG  Configuration file to parse and pass to tests
+                          [PY_TEST_CONFIG_FILE]
+                          If this is specified multiple times, all files
+                          will be parsed. In all formats except python,
+                          previous contents are preserved and the configs
+                          are merged.
+
+    --tc-format=TESTCONFIGFORMAT  Test config file format, default is
+                                  configparser ini format
+                                  [PY_TEST_CONFIG_FILE_FORMAT]
+
+    --tc=OVERRIDES        Option:Value specific overrides.
+
+    --tc-exact            Optional: Do not explode periods in override keys to
+                          individual keys within the config dict, instead treat
+                          them as config[my.toplevel.key] ala sqlalchemy.url in
+                          pylons.
+
+Contributing
+------------
+Contributions are very welcome. Tests can be run with `tox`_, please ensure
+the coverage at least stays the same before you submit a pull request.
+
+License
+-------
+
+Distributed under the terms of the `Apache Software License 2.0`_ license, "pytest-testconfig" is free and open source software
+
+
+Issues
+------
+
+If you encounter any problems, please `file an issue`_ along with a detailed description.
+
+.. _`Cookiecutter`: https://github.com/audreyr/cookiecutter
+.. _`@hackebrot`: https://github.com/hackebrot
+.. _`MIT`: http://opensource.org/licenses/MIT
+.. _`BSD-3`: http://opensource.org/licenses/BSD-3-Clause
+.. _`GNU GPL v3.0`: http://www.gnu.org/licenses/gpl-3.0.txt
+.. _`Apache Software License 2.0`: http://www.apache.org/licenses/LICENSE-2.0
+.. _`cookiecutter-pytest-plugin`: https://github.com/pytest-dev/cookiecutter-pytest-plugin
+.. _`file an issue`: https://github.com/wojole/pytest-testconfig/issues
+.. _`pytest`: https://github.com/pytest-dev/pytest
+.. _`tox`: https://tox.readthedocs.io/en/latest/
+.. _`pip`: https://pypi.org/project/pip/
+.. _`PyPI`: https://pypi.org/project
---
+++ pytest_testconfig.py
@@ -0,0 +1,214 @@
+"""Pytest-testconfig is a py.test plugin which provides passing test-specific
+(or test-run specific) configuration data to the tests being executed.
+Plugin is based on nose-testconfig plugin which provided same capabilities for
+nosetests framework
+"""
+
+import logging
+import os
+import re
+import codecs
+
+
+try:
+    import ConfigParser
+except ImportError:
+    import configparser as ConfigParser
+# Import, or define, NullHandler
+try:
+    from logging import NullHandler
+except ImportError:
+    from logging import Handler
+
+    class NullHandler(Handler):
+
+        """No-op handler."""
+
+        def emit(self, record):
+            """Intentionally do nothing."""
+            pass
+
+log = logging.getLogger(__name__)
+log.addHandler(NullHandler())
+
+
+warning = "Cannot access the test config because the plugin has not \
+been activated.  Did you specify --tc or any other command line option?"
+
+config = {}
+
+
+def tolist(val):
+    """Convert a value that may be a list or a (possibly comma-separated)
+    string into a list. The exception: None is returned as None, not [None].
+    """
+    if val is None:
+        return None
+    try:
+        # might already be a list
+        val.extend([])
+        return val
+    except AttributeError:
+        pass
+    # might be a string
+    try:
+        return re.split(r'\s*,\s*', val)
+    except TypeError:
+        # who knows...
+        return list(val)
+
+
+def merge_map(original, to_add):
+    """ Merges a new map of configuration recursively with an older one """
+    for k, v in to_add.items():
+        if isinstance(v, dict) and k in original and isinstance(original[k],
+                                                                dict):
+            merge_map(original[k], v)
+        else:
+            original[k] = v
+
+
+def load_yaml(yaml_file, encoding):
+    """ Load the passed in yaml configuration file """
+    try:
+        import yaml
+    except (ImportError):
+        raise Exception('unable to import YAML package. Can not continue.')
+    global config
+    parsed_config = yaml.load(codecs.open(yaml_file, 'r', encoding).read())
+    merge_map(config, parsed_config)
+
+
+def load_ini(ini_file, encoding):
+    """ Parse and collapse a ConfigParser-Style ini file into a two-level
+    config structure. """
+
+    global config
+    tmpconfig = ConfigParser.ConfigParser()
+    with codecs.open(ini_file, 'r', encoding) as f:
+        tmpconfig.readfp(f)
+
+    parsed_config = {}
+    for section in tmpconfig.sections():
+        parsed_config[section] = {}
+        for option in tmpconfig.options(section):
+            parsed_config[section][option] = tmpconfig.get(section, option)
+    merge_map(config, parsed_config)
+
+
+def load_python(py_file, encoding):
+    """ This will exec the defined python file into the config variable -
+    the implicit assumption is that the python is safe, well formed and will
+    not do anything bad. This is also dangerous. """
+    exec(codecs.open(py_file, 'r', encoding).read())
+
+
+def load_json(json_file, encoding):
+    """ This will use the json module to to read in the config json file.
+    """
+    import json
+    global config
+    with codecs.open(json_file, 'r', encoding=encoding) as handle:
+        parsed_config = json.load(handle)
+    merge_map(config, parsed_config)
+
+
+enabled_option = False
+name = "test_config"
+score = 1
+
+env_opt = "PY_TEST_CONFIG_FILE"
+format_option = "ini"
+encoding_option = 'utf-8'
+valid_loaders = {'yaml': load_yaml, 'ini': load_ini, 'python': load_python, 'json': load_json}
+
+
+def pytest_addoption(parser, env=os.environ):
+    """ Define the command line options for the plugin. """
+    group = parser.getgroup('test-config')
+    group.addoption('--tc-file',
+                    action='append',
+                    dest='testconfig',
+                    default=[env.get(env_opt)],
+                    help="Configuration file to parse and pass to tests"
+                            " [PY_TEST_CONFIG_FILE]")
+    group.addoption('--tc-file-encoding',
+                    action='store',
+                    dest='testconfigencoding',
+                    default=env.get('PY_TEST_CONFIG_FILE_ENCODING') or encoding_option,
+                    help="Test config file encoding, default is utf-8"
+                            " [PY_TEST_CONFIG_FILE_ENCODING]")
+    group.addoption('--tc-format',
+                    action='store',
+                    dest='testconfigformat',
+                    default=env.get('PY_TEST_CONFIG_FILE_FORMAT') or format_option,
+                    help="Test config file format, default is configparser ini format"
+                            " [PY_TEST_CONFIG_FILE_FORMAT]")
+    group.addoption('--tc',
+                    action='append',
+                    dest='overrides',
+                    default=[],
+                    help="Option:Value specific overrides.")
+    group.addoption('--tc-exact',
+                    action='store_true',
+                    dest='exact',
+                    default=False,
+                    help="Optional: Do not explode periods in override keys to "
+                        "individual keys within the config dict, instead treat them"
+                        " as config[my.toplevel.key] ala sqlalchemy.url in pylons")
+
+    # Add github marker to --help
+    parser.addini("github", "GitHub issue integration", "args")
+
+
+def pytest_configure(config):
+    """ Call the super and then validate and call the relevant parser for
+    the configuration file passed in """
+    if not config.getoption('testconfig') and not config.getoption('overrides'):
+        return
+
+    if config.getoption('testconfigformat'):
+        format_option = config.getoption('testconfigformat')
+        if format_option not in valid_loaders.keys():
+            raise Exception('%s is not a valid configuration file format' \
+                                                            % format_option)
+
+    # Load the configuration file:
+    for configfile in config.getoption('testconfig'):
+        if configfile:
+            valid_loaders[format_option](configfile,
+                                         config.getoption('testconfigencoding'))
+
+    overrides = tolist(config.getoption('overrides')) or []
+    for override in overrides:
+        keys, val = override.split(":", 1)
+        if config.getoption('exact'):
+            config[keys] = val
+        else:
+            # Create all *parent* keys that may not exist in the config
+            section = config
+            keys = keys.split('.')
+            for key in keys[:-1]:
+                if key not in section:
+                    section[key] = {}
+                section = section[key]
+
+            # Finally assign the value to the last key
+            key = keys[-1]
+            section[key] = val
+
+
+# Use an environment hack to allow people to set a config file to auto-load
+# in case they want to put tests they write through pychecker or any other
+# syntax thing which does an execute on the file.
+if 'PYTEST_TESTCONFIG_AUTOLOAD_YAML' in os.environ:
+    load_yaml(os.environ['PYTEST_TESTCONFIG_AUTOLOAD_YAML'], encoding='utf-8')
+
+if 'PYTEST_TESTCONFIG_AUTOLOAD_INI' in os.environ:
+    load_ini(os.environ['PYTEST_TESTCONFIG_AUTOLOAD_INI'], encoding='utf-8')
+
+if 'PYTEST_TESTCONFIG_AUTOLOAD_PYTHON' in os.environ:
+    load_python(os.environ['PYTEST_TESTCONFIG_AUTOLOAD_PYTHON'], encoding='utf-8')
+
+if 'PYTEST_TESTCONFIG_AUTOLOAD_JSON' in os.environ:
+    load_json(os.environ['PYTEST_TESTCONFIG_AUTOLOAD_JSON'], encoding='utf-8')
