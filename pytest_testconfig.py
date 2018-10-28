88430f36bdf9b03f88d06b8d0935a67a3a11cab3
wojole <olejarz.wojciech@gmail.com>
2018-10-28 13:35:04 +0100

    support of ~/ shortcut in --tc-file path, no error if ini not found

--- pytest_testconfig.py
+++ pytest_testconfig.py
@@ -75,7 +75,7 @@ def load_yaml(yaml_file, encoding):
     except (ImportError):
         raise Exception('unable to import YAML package. Can not continue.')
     global config
-    parsed_config = yaml.load(codecs.open(yaml_file, 'r', encoding).read())
+    parsed_config = yaml.load(codecs.open(os.path.expanduser(yaml_file), 'r', encoding).read())
     merge_map(config, parsed_config)
 
 
@@ -87,8 +87,7 @@ def load_ini(ini_file, encoding):
     tmpconfig = ConfigParser.ConfigParser()
     # Overriding optionxform method to avoid lowercase conversion
     tmpconfig.optionxform = lambda override: override
-    with codecs.open(ini_file, 'r', encoding) as f:
-        tmpconfig.read_file(f)
+    tmpconfig.read(os.path.expanduser(ini_file), encoding=encoding)
 
     parsed_config = {}
     for section in tmpconfig.sections():
@@ -102,7 +101,7 @@ def load_python(py_file, encoding):
     """ This will exec the defined python file into the config variable -
     the implicit assumption is that the python is safe, well formed and will
     not do anything bad. This is also dangerous. """
-    exec(codecs.open(py_file, 'r', encoding).read())
+    exec(codecs.open(os.path.expanduser(py_file), 'r', encoding).read())
 
 
 def load_json(json_file, encoding):
@@ -110,7 +109,7 @@ def load_json(json_file, encoding):
     """
     import json
     global config
-    with codecs.open(json_file, 'r', encoding=encoding) as handle:
+    with codecs.open(os.path.expanduser(json_file), 'r', encoding=encoding) as handle:
         parsed_config = json.load(handle)
     merge_map(config, parsed_config)
 
