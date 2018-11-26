b68bca76e087b3d9c09203275f6c1ff175af8589
wojtekolejarz <wojciech.olejarz@salesmanago.pl>
2018-11-26 23:44:21 +0100

    version number update

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='pytest-testconfig',
-    version='0.1.0',
+    version='0.1.1',
     author='Wojciech Olejarz, Bartlomiej Skrobek',
     author_email='olejarz.wojciech@gmail.com',
     maintainer='Wojciech Olejarz, Bartlomiej Skrobek',
